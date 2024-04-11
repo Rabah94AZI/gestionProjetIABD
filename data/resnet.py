ff7e40828a4bc92897182c17276c97fd293daa12
Rabah94AZI <abdouazzi91@gmail.com>
2024-04-11 21:28:11 +0200

    mise a jour de api flask

--- data/resnet.py
+++ data/resnet.py
@@ -1,84 +1,84 @@
 import pandas as pd
-import tensorflow as tf
+import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.optimizers import Adam as LegacyAdam
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
-from tensorflow.keras.applications import ResNet50V2
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.applications import MobileNetV2
+import tensorflow as tf
 
 # Charger les données
 print("------ Chargement des données ------")
 data = pd.read_csv('C:/Users/abdou/Desktop/gp/data/main_dataset.csv')
 print(data.head())
 
+# Ajustement des chemins d'accès pour qu'ils soient complets et absolus
+data['img_paths'] = data['img_paths'].apply(lambda x: os.path.join('C:/Users/abdou/Desktop/gp/data', x))
+
+# Vérifie l'existence des fichiers d'images
+data['exists'] = data['img_paths'].apply(os.path.exists)
+if not data['exists'].all():
+    print("Il y a des fichiers manquants après ajustement.")
+    missing_files = data[~data['exists']]
+    print(f"Nombre de fichiers manquants : {missing_files.shape[0]}")
+    print(missing_files['img_paths'].head())
+else:
+    print("Tous les fichiers existent après ajustement.")
+    # Procede uniquement si tous les fichiers existent
+    data = data[data['exists']]  # Garde seulement les lignes avec des fichiers existants
+
 # Ajouter une nouvelle colonne 'goodbook' basée sur la note des étoiles
 print("------ Ajout de la colonne goodbook ------")
 data['goodbook'] = (data['book_depository_stars'] >= 4.5).astype(int)
-# Convertir la colonne 'goodbook' en chaînes de caractères
 data['goodbook'] = data['goodbook'].astype(str)
 
 # Séparer les données en ensemble d'entraînement et de test
 print("------ Séparation des données train et test ------")
 train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
 
-# Générer des données d'entraînement pour les images
-print("------ Génération des données d'entrainement pour les images ------")
+# Préparer les générateurs de données
 train_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_dataframe(
     dataframe=train_data,
     x_col='img_paths',
     y_col='goodbook',
-    target_size=(64, 64),  # Taille d'entrée de ResNet
+    target_size=(128, 128),
     batch_size=32,
     class_mode='binary'
 )
 
-# Générer des données de test pour les images
-print("------ Génération des données de test pour les images ------")
 test_datagen = ImageDataGenerator(rescale=1./255)
 test_generator = test_datagen.flow_from_dataframe(
     dataframe=test_data,
     x_col='img_paths',
     y_col='goodbook',
-    target_size=(64, 64),  # Taille d'entrée de ResNet
+    target_size=(128, 128),
     batch_size=32,
     class_mode='binary'
 )
 
-# Création du modèle ResNet50V2
+# Création du modèle MobileNetV2
 print("------ Création du modèle ------")
-input_shape = (64, 64, 3)  # Taille d'entrée de ResNet
-image_input = Input(shape=input_shape, name='image_input')
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(128, 128, 3))
+base_model.trainable = False  # Gel des couches convolutives pour le fine-tuning
 
-# Utilisation de ResNet50V2 pré-entraîné
-base_model = ResNet50V2(include_top=False, input_tensor=image_input, weights='imagenet') 
-x = base_model.output
+inputs = Input(shape=(128, 128, 3))
+x = base_model(inputs, training=False)
+x = GlobalAveragePooling2D()(x)
+x = Dense(128, activation='relu')(x)
+outputs = Dense(1, activation='sigmoid')(x)
 
-# Ajout de couches entièrement connectées pour la classification binaire
-#x = tf.keras.layers.GlobalAveragePooling2D()(x)
-x = tf.keras.layers.GlobalMaxPooling2D()(x)
-
-x = Dense(256, activation='relu')(x)
-predictions = Dense(1, activation='sigmoid')(x)
-
-model = Model(inputs=image_input, outputs=predictions)
+model = Model(inputs, outputs)
 
 # Compilation du modèle
 print("------ Compilation du modèle ------")
-model.compile(optimizer=LegacyAdam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy', tf.keras.metrics.AUC(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
-#model.compile(optimizer=SGD(learning_rate=0.01, momentum=0.9), loss='binary_crossentropy', metrics=['accuracy', tf.keras.metrics.AUC(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
 
 # Callback EarlyStopping
-early_stopping = EarlyStopping(
-    monitor='val_accuracy', 
-    patience=5, 
-    verbose=1, 
-    restore_best_weights=True
-)
+early_stopping = EarlyStopping(monitor='val_accuracy', patience=5, verbose=1, restore_best_weights=True)
 
 # Entraînement du modèle
 print("------ Entraînement du modèle ------")
@@ -98,7 +98,9 @@ y_pred = (model.predict(test_generator) > 0.5).astype(int)
 f1 = f1_score(y_true, y_pred)
 print(f'Test F1-score: {f1}')
 
-#Sauvegarder le modèle
-print("------ Sauvegarde du modèle ------")
-model.save('C:/Users/abdou/Desktop/gp/data/modele_couverture_livre.h5')
-
+# Sauvegarde du modèle en format TFLite pour réduction de taille
+print("------ Sauvegarde du modèle TFLite ------")
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+with open('C:/Users/abdou/Desktop/gp/data/modele_couverture_livre.tflite', 'wb') as f:
+    f.write(tflite_model)
